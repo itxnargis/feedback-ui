37b61dd77f6c0a13162e0d349dab6b179d1eefb2
Nargis <itxnargiskhatun@gmail.com>
2023-07-27 11:43:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const ratingEls = document.querySelectorAll(".rating");
+const btnEl = document.getElementById(".btn");
+const containerEl = document.getElementById("container");
+
+let selectedRating = "";
+
+ratingEls.forEach((ratingEl) => {
+    ratingEl.addEventListener("click", (event) => {
+        removeActive();
+        selectedRating = ratingEl.innerText;
+        event.target.classList.add("active")
+        event.target.parentNode.classList.add("active")
+    });
+});
+
+btnEl.addEventListener("click", () => {
+    if (selectedRating !== "") {
+        containerEl.innerHTML = `<strong>Thank you!</strong>
+        <br>
+        <br>
+        <strong>feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support.</p>
+        `;
+    }
+});
+
+function removeActive() {
+    ratingEls.forEach((ratingEl) => {
+        ratingEl.classList.remove("active");
+    });
+}
